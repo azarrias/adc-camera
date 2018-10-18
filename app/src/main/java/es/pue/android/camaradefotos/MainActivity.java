77ad5e99514bba00682ceb007d39f39391064b69
azarrias <adolfo.zarrias@gmail.com>
2018-10-18 20:55:59 +0200

    Replace toast message by the intent to open some camera app

--- app/src/main/java/es/pue/android/camaradefotos/MainActivity.java
+++ app/src/main/java/es/pue/android/camaradefotos/MainActivity.java
@@ -1,14 +1,20 @@
 package es.pue.android.camaradefotos;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnTakePhoto;
+    private ImageView iView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +23,29 @@ public class MainActivity extends AppCompatActivity {
 
         // Associate listener to the button
         btnTakePhoto = (Button)findViewById(R.id.btnTakePhoto);
+        iView = (ImageView)findViewById(R.id.imageView);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = getResources().getString(R.string.testButton);
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                //String message = getResources().getString(R.string.testButton);
+                //Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent, 1);
+                }
             }
         };
 
         btnTakePhoto.setOnClickListener(listener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imgBmp = (Bitmap)extras.get("data");
+            iView.setImageBitmap(imgBmp);
+        }
     }
 }
