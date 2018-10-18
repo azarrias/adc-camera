4dfde29b7218353837d2b54976fa7a20166fedc5
azarrias <adolfo.zarrias@gmail.com>
2018-10-18 21:42:13 +0200

    Add send mail

--- app/src/main/java/es/pue/android/camaradefotos/MainActivity.java
+++ app/src/main/java/es/pue/android/camaradefotos/MainActivity.java
@@ -2,6 +2,7 @@ package es.pue.android.camaradefotos;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnTakePhoto;
+    private Button btnSendEmail;
     private ImageView iView;
 
     @Override
@@ -21,23 +23,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Associate listener to the button
-        btnTakePhoto = (Button)findViewById(R.id.btnTakePhoto);
         iView = (ImageView)findViewById(R.id.imageView);
+
+        // Associate listener to buttons
+        btnTakePhoto = (Button)findViewById(R.id.btnTakePhoto);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //String message = getResources().getString(R.string.testButton);
                 //Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(intent.resolveActivity(getPackageManager()) != null){
-                    startActivityForResult(intent, 1);
+                Button button = (Button)v;
+                switch(button.getId()){
+                    case R.id.btnTakePhoto:
+                        takePicture();
+                        break;
+                    case R.id.btnSendEmail:
+                        sendEmail(getString(R.string.emailDest), getString(R.string.emailSubject));
+                        break;
                 }
+
             }
         };
 
         btnTakePhoto.setOnClickListener(listener);
+
+        btnSendEmail = (Button)findViewById(R.id.btnSendEmail);
+        btnSendEmail.setOnClickListener(listener);
+    }
+
+    private void takePicture(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    private void sendEmail(String dest, String subject){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + dest));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+        startActivity(intent);
     }
 
     @Override
